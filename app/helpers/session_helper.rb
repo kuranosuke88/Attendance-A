e4b0718a6ab019acf7231359be33f57d5e0f3310
kuranosuke88 <takorou88@yahoo.co.jp>
2019-12-24 12:02:54 +0000

    ログイン機能完成

--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -4,18 +4,41 @@ module SessionHelper
     session[:user_id] = user.id
   end
   
+  # 永続的セッションを記憶する。(Userモデル参照)
+  
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+  
+  # 永続セッションを破棄
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+  
   # セッションと@current_userを破棄する
   
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
   
+  # 一時的セッションにいるユーザーを返す。
   # 現在ログインしているユーザーがいる場合オブジェを返す。
   
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
   

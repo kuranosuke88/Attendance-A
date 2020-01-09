fc40bfb0b4f6d6d0ead5fec06974b6cd4aba9ecc
kuranosuke88 <takorou88@yahoo.co.jp>
2020-01-09 14:21:58 +0000

    拠点一覧削除まで完了

--- app/controllers/bases_controller.rb
+++ app/controllers/bases_controller.rb
@@ -1,4 +1,5 @@
 class BasesController < ApplicationController
+  before_action :set_base, only: %i(destroy)
   
   def index
     @bases = Base.all
@@ -11,16 +12,26 @@ class BasesController < ApplicationController
   def create
     @base = Base.new(base_params)
     if @base.save
-      flash.now[:success] = "拠点情報を作成しました。"
-      redirect_to new
+      flash[:success] = "拠点情報を作成しました。"
+      redirect_to bases_url
     else
       render :new
     end
+  end
+  
+  def destroy
+    @base.destroy
+    flash[:success] = "#{@base.base_name}のデータを削除しました。"
+    redirect_to bases_url
   end
   
   private
     
     def base_params
       params.require(:base).permit(:base_number, :base_name, :attendance_type)
+    end
+    
+    def set_base
+      @base = Base.find(params[:id])
     end
 end
